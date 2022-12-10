907b8e89ec3c3b9181d3f904b1463e04d9d7ed00
nivedhanr <nivedharenahan@gmail.com>
2022-12-10 18:45:38 +0530

    first

---
+++ src/test/java/com/example/complaintspingboot/ComplaintspingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.complaintspingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintspingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
